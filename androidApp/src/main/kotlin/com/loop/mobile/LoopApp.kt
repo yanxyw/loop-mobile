f825b6aa7626726592eefca7a62f6e46eb073ff5
Evelyn <96553159+evelyn-2022@users.noreply.github.com>
2025-03-21 23:27:00 +0000

    Refactor koin initializer

--- androidApp/src/main/kotlin/com/loop/mobile/LoopApp.kt
+++ androidApp/src/main/kotlin/com/loop/mobile/LoopApp.kt
@@ -1,17 +1,12 @@
 package com.loop.mobile
 
 import android.app.Application
-import com.loop.mobile.di.appModule
-import org.koin.android.ext.koin.androidContext
-import org.koin.core.context.startKoin
+import com.loop.mobile.di.initKoin
 
 class LoopApp : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        startKoin {
-            androidContext(this@LoopApp)
-            modules(appModule)
-        }
+        initKoin()
     }
 }
