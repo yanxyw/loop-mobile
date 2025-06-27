a7089bd98e231f0f159c134025ee2f19a6128692
Kim <evelyn20220101@gmail.com>
2025-06-27 21:17:21 +0100

    Remove redundant initToken

--- androidApp/src/main/kotlin/com/loop/mobile/LoopApp.kt
+++ androidApp/src/main/kotlin/com/loop/mobile/LoopApp.kt
@@ -1,15 +1,12 @@
 package com.loop.mobile
 
 import android.app.Application
-import com.loop.mobile.data.local.initTokenStorage
 import com.loop.mobile.di.initKoin
 import org.koin.android.ext.koin.androidContext
 
 class LoopApp : Application() {
     override fun onCreate() {
         super.onCreate()
-
-        initTokenStorage(this)
 
         initKoin(
             appContext = { androidContext(this@LoopApp) },
--- shared/src/androidMain/kotlin/com/loop/mobile/di/KoinInitializer.android.kt
+++ shared/src/androidMain/kotlin/com/loop/mobile/di/KoinInitializer.android.kt
@@ -5,7 +5,7 @@ import org.koin.core.context.startKoin
 
 fun initKoin(
     appContext: (KoinApplication.() -> Unit)? = null,
-    baseUrl: String = "http://localhost:8080/api/v1"
+    baseUrl: String = "http://10.0.2.2:8080/api/v1"
 ) {
     startKoin {
         appContext?.invoke(this)
